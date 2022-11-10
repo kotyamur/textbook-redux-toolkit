0aac7983e9199e77435af1bb9dc60810b5d0e3c4
Kateryna Kleshch <ketrins1993@gmail.com>
2022-11-10 23:38:24 +0200

    add dispatch

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,17 +1,42 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'components/Button/Button';
+// Импортируем объект значений фильтра
 import { statusFilters } from '../../redux/constants';
+// Импортируем генератор экшена
+import { setStatusFilter } from '../../redux/actions';
 import { getStatusFilter } from '../../redux/selectors';
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // Получаем ссылку на функцию отправки экшенов
+  const dispatch = useDispatch();
+  // Получаем значение фильтра из состояния Redux
   const filter = useSelector(getStatusFilter);
+
+  // Вызываем генератор экшена и передаём значение фильтра
+  // Отправляем результат - экшен изменения фильтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
 
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === statusFilters.all}>All</Button>
-      <Button selected={filter === statusFilters.active}>Active</Button>
-      <Button selected={filter === statusFilters.completed}>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,19 @@
-import { Button } from "components/Button/Button";
-import css from "./TaskForm.module.css";
+import { Button } from 'components/Button/Button';
+import css from './TaskForm.module.css';
+import { useDispatch } from 'react-redux';
+// Импортируем генератор экшена
+import { addTask } from '../../redux/actions';
 
 export const TaskForm = () => {
-  const handleSubmit = (event) => {
+  // Получаем ссылку на функцию отправки экшенов
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    // Вызываем генератор экшена и передаем текст задачи для поля payload
+    // Отправляем результат - экшен создания задачи
+    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
 
